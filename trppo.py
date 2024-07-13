77a7ea9d67dfef00596831e0c86245c3e2561678
asddzxcc1856 <t7878780@gmail.com>
2024-07-13 11:20:48 +0800

    Fix bugs

--- trppo.py
+++ trppo.py
@@ -276,10 +276,10 @@ class TRPPO(OnPolicyAlgorithm):
                 with th.no_grad():
                     last_stage_prob = old_model.policy.get_distribution(rollout_data.observations).distribution.probs
                     last_stage_values, last_stage_log_prob, last_stage_entropy = old_model.policy.evaluate_actions(rollout_data.observations, actions)
-                    delta_value = values - last_stage_values
+                    delta_value = values.unsqueeze(dim=-1) - last_stage_values
                     lambd = th.clip(delta_value, min=-0.05, max=0) * (-1e3)
                     lambd_root = lambd ** 0.5
-                transfer_regularization = F.mse_loss(lambd_root * prob, lambd_root * last_stage_prob)
+                transfer_regularization = F.mse_loss(th.multiply(prob, lambd_root), th.multiply(last_stage_prob, lambd_root))
                 lambds.append(th.mean(lambd).item())
 
                 loss = policy_loss + self.ent_coef * entropy_loss + self.vf_coef * value_loss + transfer_regularization
