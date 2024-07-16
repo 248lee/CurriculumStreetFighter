e69aab601d59797da4201e8218411eddf70ca0d5
JohnLee <fantasy10final@gmail.com>
2024-07-16 23:35:25 +0800

    Set parameters of stage 2

--- train_stage2forserver.py
+++ train_stage2forserver.py
@@ -144,7 +144,8 @@ def main():
         model = TRPPO(
             "CnnPolicy",
             env,
-            old_model_name="value_transfer_final.zip",
+            old_model_name_value="value_transfer_final.zip",
+            old_model_name_policy="ppo_chun_vs_ryu_john_s1_exp2_final.zip",
             transfer_lambd=transfer_lambd,
             device="cuda", 
             verbose=1,
@@ -181,7 +182,7 @@ def main():
         param.requires_grad = True
 
     checkpoint_interval = 31250 * 4 # checkpoint_interval * num_envs = total_steps_per_checkpoint
-    ExperimentName = "ppo_chun_vs_ryu_s2_exp2_real"
+    ExperimentName = "ppo_chun_vs_ryu_s2_exp2_unfreeze_value"
     checkpoint_callback = CheckpointCallback(save_freq=checkpoint_interval, save_path=save_dir, name_prefix=ExperimentName)
 
     # Writing the training logs from stdout to a file
